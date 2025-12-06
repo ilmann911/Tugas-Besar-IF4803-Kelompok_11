e36f43a32d05ea30a90065d65ee3c31cd037222e
ilmann911 <mohamadilmansyah09@gmail.com>
2025-12-06 19:36:19 +0700

    menambahkan primitif dan implementasi subprogram

--- kejaksaan/terdakwa.h
+++ kejaksaan/terdakwa.h
@@ -2,6 +2,7 @@
 #define TERDAKWA_H_INCLUDED
 
 #include <iostream>
+using namespace std;
 
 struct infotypeTerdakwa {
     string noRegistrasi;
@@ -14,15 +15,24 @@ typedef struct elmTerdakwa *adrTerdakwa;
 struct elmTerdakwa {
     infotypeTerdakwa info;
     adrTerdakwa next;
-    adrTerdakwa prev;
 };
 
 struct ListTerdakwa {
     adrTerdakwa first;
-    adrTerdakwa last;
 };
 
-using namespace std;
+void createListTerdakwa(ListTerdakwa &L);
+adrTerdakwa createElmTerdakwa(infotypeTerdakwa x);
+void insertFirstTerdakwa(ListTerdakwa &L, adrTerdakwa P);
+void insertLastTerdakwa(ListTerdakwa &L, adrTerdakwa P);
+void insertAfterTerdakwa(ListTerdakwa &L, adrTerdakwa P, adrTerdakwa Prec);
+void deleteFirstTerdakwa(ListTerdakwa &L, adrTerdakwa &P);
+void deleteLastTerdakwa(ListTerdakwa &L, adrTerdakwa &P);    
+void deleteAfterTerdakwa(ListTerdakwa &L, adrTerdakwa &P, adrTerdakwa Prec);
+adrTerdakwa findTerdakwa(ListTerdakwa L, string noRegistrasi);
+void viewTerdakwa(ListTerdakwa L);
+void countTerdakwa(ListTerdakwa L, int &count);
+
 
 
 #endif // TERDAKWA_H_INCLUDED
--- kejaksaan/terdakwa_103012400296.cpp
+++ kejaksaan/terdakwa_103012400296.cpp
@@ -5,3 +5,40 @@
 #include <iostream>
 
 using namespace std;
+
+void createListTerdakwa(ListTerdakwa &L){
+    L.first = NULL;
+}
+adrTerdakwa createElmTerdakwa(infotypeTerdakwa x){
+    adrTerdakwa P = new elmTerdakwa;
+    P->info = x;
+    P->next = NULL;
+    return P;
+}
+void insertFirstTerdakwa(ListTerdakwa &L, adrTerdakwa P){
+    if(L.first == NULL){
+        L.first = P;
+    } else {
+        P->next = L.first;
+        L.first = P;
+    }
+}
+void insertLastTerdakwa(ListTerdakwa &L, adrTerdakwa P){
+    if(L.first == NULL){
+        L.first = P;
+    } else {
+        adrTerdakwa Q = L.first;
+        while(Q->next != NULL){
+            Q = Q->next;
+        }
+        Q->next = P;
+    }
+}
+void insertAfterTerdakwa(ListTerdakwa &L, adrTerdakwa P, adrTerdakwa Prec){
+}
+void deleteFirstTerdakwa(ListTerdakwa &L, adrTerdakwa &P);
+void deleteLastTerdakwa(ListTerdakwa &L, adrTerdakwa &P);    
+void deleteAfterTerdakwa(ListTerdakwa &L, adrTerdakwa &P, adrTerdakwa Prec);
+adrTerdakwa findTerdakwa(ListTerdakwa L, string noRegistrasi);
+void viewTerdakwa(ListTerdakwa L);
+void countTerdakwa(ListTerdakwa L, int &count);
